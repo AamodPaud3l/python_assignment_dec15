91efeab4471fbc82b4764aaf0b74b9cef587d98a
AamodPaud3l <paudelaamod@gmail.com>
2017-12-17 19:10:39 +0545

    Unpack from list and print it

---
+++ unpack_from_list.py
@@ -0,0 +1,13 @@
+#Program to return a list from a function and unpack it
+mix = ("Hello", 45, 23.3)
+def random(tup):
+        return tup
+
+x, y, z = random(mix)
+print(x)
+print(y)
+print(z)
+
+
+
+
